c69fbd59df0296fc1531c72646a865af0ddfe4b7
zm24acgRiyan <zm24acg@herts.ac.uk>
2025-04-22 03:32:25 +0530

    Added number validation

--- src/clinic/Validations.java
+++ src/clinic/Validations.java
@@ -1,4 +1,13 @@
 package clinic;
 
 public class Validations {
+
+    public boolean validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
